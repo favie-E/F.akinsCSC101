ddafbb9555d4a95f1d1a0c4e185b41ddb55a6598
favie-E <favour.akinbobola@pau.edu.ng>
2024-10-26 01:39:43 +0100

    (My first commit)

---
+++ week-1/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+	println!(); //print just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("My name is {}. I am a student of SEN", "Favour Akinbobola")
+}
